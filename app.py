2e15990f1deead665c797f6cfbb9cced4e3de6e8
ojdgonzo <70494711+ojdgonzo@users.noreply.github.com>
2023-05-16 09:14:02 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from werkzeug.utils import secure_filename
+import os
+
+# Import required libraries 
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# importing pickle
+import pickle
+
+
+app = Flask(__name__)
+app.secret_key = '9sda01133na-djm1'
+
+@app.route('/')
+def index():
+    return render_template('submission.html')
+
+
+@app.route('/prediction', methods = ['POST', 'GET'])
+def prediction():
+    bigbudget = float(request.form['BigBudget'])
+    unpickled = pickle.load(open('linearModel.pkl', 'rb'))
+    prediction = unpickled.predict([[bigbudget]])
+    # print(prediction)
+    return render_template('prediction.html', prediction = prediction[0][0])
+
+if __name__ == '__main__':
+    app.run(debug = True)
