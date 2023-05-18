0fb3ac30732d8ad4d9b46403a49de48edd92e758
ojdgonzo <70494711+ojdgonzo@users.noreply.github.com>
2023-05-17 20:39:58 -0700

    Updated app.py again...

--- app.py
+++ app.py
@@ -1,24 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from werkzeug.utils import secure_filename
-import os
-
-# Import required libraries 
-import numpy as np
-import pandas as pd
-from sklearn.linear_model import LinearRegression
+# Importing the proper flask libraries
+from flask import Flask, render_template, request
 
 # importing pickle
 import pickle
 
-
+# Creating our flask project
 app = Flask(__name__)
 app.secret_key = '9sda01133na-djm1'
 
+# homepage
 @app.route('/')
 def index():
     return render_template('submission.html')
 
-
+# prediction page
+# it will take in any numerical value to see what the possible revenue value
+# using pickle to "unpickle" our file and pull our linear regression prediction 
 @app.route('/prediction', methods = ['POST', 'GET'])
 def prediction():
     bigbudget = float(request.form['BigBudget'])
@@ -27,5 +24,6 @@ def prediction():
     # print(prediction)
     return render_template('prediction.html', prediction = prediction[0][0].round(2))
 
+# running our project
 if __name__ == '__main__':
     app.run(debug = True)
